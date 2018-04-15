9f359b802a54b2bd9148753288a99bc47eae1373
mandrewwow <38403619+mandrewwow@users.noreply.github.com>
2018-04-15 21:27:48 +0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace Лаба_6__rez
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.stopbutton5 = new System.Windows.Forms.Button();
+            this.continuebutton4 = new System.Windows.Forms.Button();
+            this.endbutton3 = new System.Windows.Forms.Button();
+            this.createbutton2 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.list = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(470, 388);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.stopbutton5);
+            this.groupBox1.Controls.Add(this.continuebutton4);
+            this.groupBox1.Controls.Add(this.endbutton3);
+            this.groupBox1.Controls.Add(this.createbutton2);
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.list);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(488, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(260, 426);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Управление";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(82, 23);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(107, 20);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Работающие";
+            // 
+            // stopbutton5
+            // 
+            this.stopbutton5.Location = new System.Drawing.Point(16, 221);
+            this.stopbutton5.Name = "stopbutton5";
+            this.stopbutton5.Size = new System.Drawing.Size(238, 23);
+            this.stopbutton5.TabIndex = 12;
+            this.stopbutton5.Text = "Стоп";
+            this.stopbutton5.UseVisualStyleBackColor = true;
+            this.stopbutton5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // continuebutton4
+            // 
+            this.continuebutton4.Location = new System.Drawing.Point(16, 192);
+            this.continuebutton4.Name = "continuebutton4";
+            this.continuebutton4.Size = new System.Drawing.Size(238, 23);
+            this.continuebutton4.TabIndex = 11;
+            this.continuebutton4.Text = "Продолжить";
+            this.continuebutton4.UseVisualStyleBackColor = true;
+            this.continuebutton4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // endbutton3
+            // 
+            this.endbutton3.Location = new System.Drawing.Point(16, 163);
+            this.endbutton3.Name = "endbutton3";
+            this.endbutton3.Size = new System.Drawing.Size(238, 23);
+            this.endbutton3.TabIndex = 10;
+            this.endbutton3.Text = "Завершить поток";
+            this.endbutton3.UseVisualStyleBackColor = true;
+            this.endbutton3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // createbutton2
+            // 
+            this.createbutton2.Location = new System.Drawing.Point(16, 134);
+            this.createbutton2.Name = "createbutton2";
+            this.createbutton2.Size = new System.Drawing.Size(238, 23);
+            this.createbutton2.TabIndex = 9;
+            this.createbutton2.Text = "Создать поток";
+            this.createbutton2.UseVisualStyleBackColor = true;
+            this.createbutton2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.AllowDrop = true;
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(16, 260);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(238, 24);
+            this.comboBox2.TabIndex = 8;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(147, 302);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(60, 20);
+            this.numericUpDown1.TabIndex = 7;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(33, 302);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 23);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Радиус";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label3.UseMnemonic = false;
+            // 
+            // list
+            // 
+            this.list.FormattingEnabled = true;
+            this.list.Location = new System.Drawing.Point(16, 46);
+            this.list.Name = "list";
+            this.list.Size = new System.Drawing.Size(238, 82);
+            this.list.TabIndex = 4;
+            this.list.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(37, 333);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 23);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Цвет";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(147, 333);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.AllowDrop = true;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox1.Location = new System.Drawing.Point(16, 391);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(238, 24);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(16, 365);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(238, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Приоритет главного потока";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(760, 429);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Потоки";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox list;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button endbutton3;
+        private System.Windows.Forms.Button createbutton2;
+        private System.Windows.Forms.Button stopbutton5;
+        private System.Windows.Forms.Button continuebutton4;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;//подключение потоков
+using System.Diagnostics;//это для того чтобы все потоки на одном ядре были
+
+namespace Лаба_6__rez
+{
+  
+    public partial class Form1 : Form
+    {
+        private Random rnd = new Random();
+        private object locker = new object();
+        int count = -1;//счетчик созданных потоков
+        const int kolvo = 100;//максимальное количество потоков
+        Thread[] potok = new Thread[kolvo];//массив на 100 потоков
+        String[] prioritet = new String[5];//массив имен приоритетов
+        SolidBrush[] kist = new SolidBrush[kolvo];//100 разных кистей для каждого потока
+        int[] radius = new int[kolvo];//радиусы
+        String[] names = new String[kolvo];//массив имен потоков для текстового поля
+        Graphics[] g = new Graphics[kolvo];//массив для рисования
+       // static Mutex mutexObj = new Mutex();//создаем новый мьютекс
+        public Form1()
+        {
+            InitializeComponent();//Метод, который инициализирует все компоненты, расположенные на форме: поля, кнопки, меню, переключатели и т. п.
+            for (int i = 0; i < kolvo; i++)
+                g[i] = pictureBox1.CreateGraphics();//задаем поверхность для рисования
+            prioritet[0] = "Lowest"; //имена для полей комбобоксов
+            prioritet[1] = "BelowNormal";
+            prioritet[2] = "Normal";
+            prioritet[3] = "AboveNormal";
+            prioritet[4] = "Highest";
+            for(int i=0;i<5;i++)
+            {
+                comboBox1.Items.Add(prioritet[i]);
+                comboBox2.Items.Add(prioritet[i]);
+            }
+            numericUpDown1.Value = (int)(10);
+            for (int i = 0; i < kolvo; i++)
+                radius[i] = new int();
+            comboBox1.Text = Convert.ToString(Thread.CurrentThread.Priority);
+
+        }
+        public static void SetThreadAffinity(int mask)
+        {
+            int threadId = GetCurrentThreadId();//чтобы выполнялись на одном ядре
+            var process = Process.GetCurrentProcess();
+            process.Refresh();
+
+            var currentThread = process.Threads
+                .OfType<ProcessThread>()
+                .Where(t => t.Id == threadId)
+                .Single();
+            currentThread.ProcessorAffinity = new IntPtr(mask);
+        }
+        [System.Runtime.InteropServices.DllImport("kernel32.dll")] static extern int GetCurrentThreadId();
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            count++;
+            Random col = new Random();
+            kist[count] = new SolidBrush(Color.FromArgb(col.Next(0, 255), col.Next(0, 255), col.Next(0, 255)));//создаю кисть 
+            names[count] = Convert.ToString("Potok " + (count));
+            list.Items.Add(names[count]);//посылаю значение в листбокс
+            radius[count] = Convert.ToInt32(20);//значение радиуса из поля
+            
+            potok[count] = new Thread(drow);//создание потока
+            potok[count].Start();//запуск потока
+            
+        }
+        private void drow()
+        {
+           
+            int i = count;
+            SetThreadAffinity(1);//чтобы было на одном ядре
+           
+            g[i] = pictureBox1.CreateGraphics();//чтобы рисовало только в области(picturebox1)
+            int x, y;
+            
+            while(true)
+            {
+                    lock(locker)
+                { 
+                //mutexObj.WaitOne();//ожидает до тех пор, пока не будет получен мьютекс
+                    x = rnd.Next(0, 470);
+                    y = rnd.Next(0, 410);
+                
+                    g[i].FillEllipse(kist[i], x, y, radius[i], radius[i]);//интервал между потоками
+                }
+                //mutexObj.ReleaseMutex();//освобождаем мьютекс
+            }
+             
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i <= count; i++)
+                if (list.Text == names[i]) 
+                {
+                    list.SelectedItem = names[0];
+                    list.Items.Remove(names[i]);
+                    potok[i].Abort(); //остановка потока
+                }
+                   
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i <= count; i++)
+                    if (list.Text == names[i])
+                        potok[i].Suspend();//останавливаю поток
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                button1.BackColor = colorDialog1.Color;//задаю цвет кнопки с помошью которой открывается менюшка цветов
+                for (int i = 0; i <= count; i++)
+                    if (list.Text == names[i])
+                    {
+                        kist[i].Color = colorDialog1.Color;//меняю цвет кисти потока
+                        potok[i].Resume();//продолжаю работу потока
+                    }
+            }
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            for (int i = 0; i <= count; i++)
+                if (list.Text == names[i])
+                    radius[i] = Convert.ToInt32(numericUpDown1.Value);
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int prior = -1;
+            if (Convert.ToString(list.SelectedItem) != "")
+            {
+                for (int i = 0; i <= count; i++)
+                    if (list.Text == names[i])
+                    {
+                        prior = i;
+                        button1.BackColor = kist[prior].Color;
+                        numericUpDown1.Value = radius[prior];
+                    }
+                comboBox2.Text = Convert.ToString(potok[prior].Priority);//присваиваю полю комбобокс приоритет выбранного потока
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            for (int i = 0; i <= count; i++)
+                if (list.Text == names[i])
+                {
+                    switch (comboBox2.Text)
+                    {
+                        case "Lowest": potok[i].Priority = ThreadPriority.Lowest; break;
+                        case "BelowNormal": potok[i].Priority = ThreadPriority.BelowNormal; break;
+                        case "Normal": potok[i].Priority = ThreadPriority.Normal; break;
+                        case "AboveNormal": potok[i].Priority = ThreadPriority.AboveNormal; break;
+                        case "Highest": potok[i].Priority = ThreadPriority.Highest; break;
+                        default: ; break;
+                    }
+                }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i <= count; i++)
+                if (list.Text == names[i])
+                    potok[i].Suspend();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i <= count; i++)
+                if (list.Text == names[i])
+                    potok[i].Resume();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox1.Text)
+            {
+                case "Lowest": Thread.CurrentThread.Priority = ThreadPriority.Lowest; break;
+                case "BelowNormal": Thread.CurrentThread.Priority = ThreadPriority.BelowNormal; break;
+                case "Normal": Thread.CurrentThread.Priority = ThreadPriority.Normal; break;
+                case "AboveNormal": Thread.CurrentThread.Priority = ThreadPriority.AboveNormal; break;
+                case "Highest": Thread.CurrentThread.Priority = ThreadPriority.Highest; break;
+                default: ; break;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            for (int i = 0; i <= count; i++)
+                potok[i].Abort();
+        }
+        
+    }
+}
